a001d3941787cbb6d8641771a07dc7b34d1b86e8
yasith heshan <yasith.heshan@pickme.lk>
2025-09-05 20:12:54 +0530

    fix(users): add await to bcrypt.compare in verifyUser method for proper password validation

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -15,7 +15,7 @@ export class UsersService {
 
     async verifyUser(email: string, password: string) {
         const user =  await this.usersRepository.findOne({email})
-        const passwordIsValid =  bcrypt.compare(password,user.password);
+        const passwordIsValid = await bcrypt.compare(password,user.password);
         if (!passwordIsValid){
             throw new UnauthorizedException('Credentials are not valid');
         }
